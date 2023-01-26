846838dc9564aa876ab77fd0f62247665612c154
Hal Shin <hal.shin@alumni.ubc.ca>
2023-01-25 20:01:57 -0800

    Use prod env

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -1,3 +1,5 @@
 import PocketBase from 'pocketbase';
 
-export const client = new PocketBase('http://127.0.0.1:8090');
+export const client = new PocketBase(
+  import.meta.env.PROD ? 'http://146.190.43.123' : 'http://127.0.0.1:8090'
+);
